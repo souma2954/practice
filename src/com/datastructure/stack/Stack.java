17c3cf535fd920447722a7a2f47cf82e9982e734
Soumadeep <soumadeep1985@gmail.com>
2020-08-13 16:53:41 +0530

    Stack implementations

---
+++ src/com/datastructure/stack/Stack.java
@@ -0,0 +1,10 @@
+package com.datastructure.stack;
+
+public class Stack {
+    private int[] data;
+    public void push(){}
+    public int pop(){ return 0;}
+    public int peek(){ return 0;}
+
+    public boolean isEmpty(){return false;}
+}
